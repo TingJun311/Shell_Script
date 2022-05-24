bc04e95aba8c96ecb837540ca7a0e3ad2e37102f
Chew TingJun <chewtj311@LAPTOP-IS2HE4Q.localdomain>
2022-05-24 23:24:44 +0800

    changed indentation of the code

--- shell.sh
+++ shell.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
  MY_NAME=""
-echo What is your name?
-read MY_NAME 
-echo "Hello $MY_NAME "
+    echo What is your name?
+    read MY_NAME 
+    echo "Hello $MY_NAME "
 
 #----------------------------------------------------------------------------------------------------
 for i in hello 1 * 2 goodbye 
 do
-  echo "Looping ... i is set to $i"
+    echo "Looping ... i is set to $i"
 done
 
 #-------------------------------------------------------------------------------------------------------
 INPUT_STRING=hello
 while [ "$INPUT_STRING" != "bye" ]
 do
-  echo "Please type something in (bye to quit)"
-  read INPUT_STRING
-  echo "You typed: $INPUT_STRING"
+    echo "Please type something in (bye to quit)"
+    read INPUT_STRING
+    echo "You typed: $INPUT_STRING"
 done
 
 
@@ -26,12 +26,14 @@ done
 a=0
 while [ "$a" -lt 10 ]
 do 
-	b="$a"
+    b="$a"
+
 	while [ "$b" -ge 0 ]
 	do
 		echo -n "$b "
 		b=`expr $b - 1`
 	done
+
 	echo 
 	a=`expr $a + 1`
 done
