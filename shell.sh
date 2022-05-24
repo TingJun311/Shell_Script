ef7e608286cc17400ec077dc48bf39fb488a36a1
Isaac <swengineerwithisaac@gmail.com>
2022-05-24 18:19:05 +0800

    Added comments and nested loop

--- shell.sh
+++ shell.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-MY_NAME=""
+ MY_NAME=""
 echo What is your name?
 read MY_NAME 
 echo "Hello $MY_NAME "
 
+#----------------------------------------------------------------------------------------------------
 for i in hello 1 * 2 goodbye 
 do
   echo "Looping ... i is set to $i"
 done
 
+#-------------------------------------------------------------------------------------------------------
 INPUT_STRING=hello
 while [ "$INPUT_STRING" != "bye" ]
 do
@@ -17,4 +19,24 @@ do
   read INPUT_STRING
   echo "You typed: $INPUT_STRING"
 done
+
+
+
+# ----------------------------------------------------------------------------------------------------
+a=0
+while [ "$a" -lt 10 ]
+do 
+	b="$a"
+	while [ "$b" -ge 0 ]
+	do
+		echo -n "$b "
+		b=`expr $b - 1`
+	done
+	echo 
+	a=`expr $a + 1`
+done
+
+
+
+
 
